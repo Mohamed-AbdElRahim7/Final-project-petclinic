a73e6f94dca4efbd0602d56b496cc8e6c8c50dc1
Mohamed Abd El_Rahim <161028740+Mohamed-AbdElRahim7@users.noreply.github.com>
2025-11-15 23:21:43 +0200

    Update backend.tf

--- terraform-k8s-infrastructure/backend.tf
+++ terraform-k8s-infrastructure/backend.tf
@@ -1,8 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket         = "petclinic-terraform-backend-atos "
-    key            = "terraform/state/infra.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
+  backend "local" {
+    path = "terraform.tfstate"
   }
 }
